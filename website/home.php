9f2e496b442701bb6ee028a4fd15e89d55bce144
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2023-02-18 19:40:18 +0600

    practice

--- website/home.php
+++ website/home.php
@@ -20,14 +20,19 @@
 
 <body>
     <!-- header section starts  -->
-    <section class="head">
+    <section class="header">
+
         <a href="home.php" class="logo">travel.</a>
+
         <nav class="navbar">
             <a href="home.php">home</a>
             <a href="about.php">about</a>
             <a href="package.php">package</a>
             <a href="book.php">book</a>
         </nav>
+        <!-- it shows the bar  -->
+        <div id="menu-btn" class="fas fa-bars"></div>
+
     </section>
     <!-- header section ends  -->
 
@@ -42,7 +47,47 @@
 
 
 
+    <!-- footer section starts -->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3> Quick links </h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+            </div>
 
+            <div class="box">
+                <h3> Extra links </h3>
+                <a href="#"><i class="fas fa-angle-right"></i> Ask Question </a>
+                <a href="#"><i class="fas fa-angle-right"></i> About Us </a>
+                <a href="#"><i class="fas fa-angle-right"></i> Privacy Policy </a>
+                <a href="#"><i class="fas fa-angle-right"></i> Terms of Use </a>
+
+            </div>
+            <div class="Box">
+                <h3>Contact Info</h3>
+                <a herf="#"><i class="fas fa-phone"></i> +880 1971 306 641 </a>
+                <a herf="#"><i class="fas fa-phone"></i> +880 1621 887 399 </a>
+                <a herf="#"><i class="fas fa-envelope"></i> hk30532@gmail.com </a>
+                <a herf="#"><i class="fas fa-map"></i> xy,Mohakhali,Dhaka-1213 </a>
+
+            </div>
+            <div class="box">
+                <h3>Follow Us</h3>
+                <a herf="#"> <i class="fab fa-facebook"></i> Facebook </a>
+                <a herf="#"> <i class="fab fa-twitter"></i> Twitter </a>
+                <a herf="#"> <i class="fab fa-instagram"></i> Instagram </a>
+                <a herf="#"> <i class="fab fa-linkedin"></i> Liknedid </a>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- footer section ends -->
 
     <!-- swiper js link  -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
